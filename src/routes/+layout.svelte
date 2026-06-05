9db72746dc9d743b492fb7ef7ed2f458cf71ee74
DEEPJYOTI <jyotirmoybaishya10@gmail.com>
2026-06-05 14:49:31 +0530

    Initial commit via GitFlow

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import '../app.css';
+  import Toast from '$lib/components/Toast.svelte';
+  import { showToast } from '$lib/stores/toast.svelte.js';
+  import { onMount } from 'svelte';
+  
+  onMount(() => {
+    window.showToast = showToast;
+  });
+</script>
+
+<slot />
+<Toast />
